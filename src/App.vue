d4c92d28531643e8c8c7f8802f690cb380ce7fdf
Fabian <fafunas@gmail.com>
2021-12-13 14:53:22 -0300

    Modify ProductList

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/admin">Admin</router-link>
+      <router-link to="/productlist">Listado</router-link>
     </div>
     <router-view/>
   </div>
--- src/components/Listado.vue
+++ src/components/Listado.vue
@@ -1,53 +1,42 @@
 <template>
-  <v-card
-    class="mx-auto ma-4"
-    max-width="300"
-  >
-    <v-img
-      class="white--text align-end"
-      height="200px"
-      :src="portada"
-    >
-      <v-card-title>{{titulo}}</v-card-title>
+  <v-card class="mx-auto ma-4" max-width="300">
+    <v-img class="white--text align-end" height="200px">
+      <v-card-title>{{ producto.name }}</v-card-title>
     </v-img>
 
-    <v-card-subtitle class="pb-0">
-      ${{costo}}
-    </v-card-subtitle>
+    <v-card-subtitle class="pb-0"> ${{ producto.costo }} </v-card-subtitle>
 
-    <v-card-text class="text--primary ">
-      
-      <div>{{descripcion}}</div>
+    <v-card-text class="text--primary">
+      <div>{{ producto.descripcion }}</div>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn
-        outlined
-        rounded
-        text
-      >
-        Agregar
-      </v-btn>
+      <v-btn outlined rounded text @click="consulta()"> Agregar </v-btn>
     </v-card-actions>
-     
- 
   </v-card>
 </template>
 
 <script>
 export default {
-  props: {
-    titulo: String,
-    portada: String,
-    costo: Number,
-    descripcion: String,
-  },
   data() {
     return {
       show: false,
+      
     };
   },
+  
+  props:{
+    producto: Array
+    
+  },
+
+  methods:{
+    consulta(){
+      console.log("Esto deberia ser ",this.producto)
+    }
+  }
 };
+
 </script>
 
 <style lang="scss" scoped></style>
